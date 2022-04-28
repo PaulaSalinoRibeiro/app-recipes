408cb7f097ec08f236810566cb9c1b37bcbd2bfe
Raphael Lyra Perazzo Leite Galvão <raphael.perazzo@gmail.com>
2022-04-28 18:59:45 -0300

    dia 03 concluido, todos nós sobrevivemos ao processo

--- src/Redux/actions/index.js
+++ src/Redux/actions/index.js
@@ -1,3 +1,9 @@
 export const VALIDATION_USER = 'VALIDATION_USER';
+export const SAVE_FOODS = 'SAVE_FOODS';
+export const SAVE_DRINKS = 'SAVE_DRINKS';
 
 export const actionUser = (value) => ({ type: VALIDATION_USER, value });
+
+export const actionSaveFoods = (value) => ({ type: SAVE_FOODS, value });
+
+export const actionSaveDrinks = (value) => ({ type: SAVE_DRINKS, value });
--- src/Redux/reducers/index.js
+++ src/Redux/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import userReducer from './user';
+import saveFoods from './saveFoods';
+import searchDrinks from './searchDrinks';
 
 const rootReducer = combineReducers({
   userReducer,
+  saveFoods,
+  searchDrinks,
 });
 
 export default rootReducer;
