689ebd2d508797c499f00e8bb6b9b303509bd168
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-05-02 11:57:41 -0300

    profile page requests finnesh

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+import '../styles/Profile.css';
+
 function Profile() {
+  const [email, setEmail] = useState('');
+
+  const history = useHistory();
+
+  useEffect(() => {
+    const userEmail = JSON.parse(localStorage.getItem('user'));
+    setEmail(userEmail.email);
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div>
       <Header
         text="Profile"
       />
+      <div className="container-profile">
+        <p
+          data-testid="profile-email"
+        >
+          {email}
+        </p>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ handleLogout }
+        >
+          Logout
+        </button>
+      </div>
       <Footer />
     </div>
   );
