44af63dec7479c0e31883c592db45993ffb970aa
Raphael Lyra Perazzo Leite Galvão <raphael.perazzo@gmail.com>
2022-05-05 18:04:20 -0300

    eu vou enfartar, req 67 a 72 passando

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import '../styles/Explore.css';
 
 function Explore() {
+  const history = useHistory();
   return (
-    <div>
+    <div className="div-page-explore">
       <Header
         text="Explore"
       />
+      <h1 data-testid="page-title">Explore</h1>
+      <div className="div-explore">
+        <button
+          className="explore"
+          onClick={ () => history.push('/explore/foods') }
+          data-testid="explore-foods"
+          type="button"
+        >
+          Explore Foods
+        </button>
+        <button
+          className="explore"
+          onClick={ () => history.push('/explore/drinks') }
+          data-testid="explore-drinks"
+          type="button"
+        >
+          Explore Drinks
+        </button>
+      </div>
       <Footer />
     </div>
   );
