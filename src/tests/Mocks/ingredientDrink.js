b12719b566d443d90e073e8f0591a029284d901f
DannyElis89 <dannyelis89@gmail.com>
2022-05-10 18:14:31 -0300

    salvando progresso

---
+++ src/tests/Mocks/ingredientDrink.js
@@ -0,0 +1,24 @@
+const mockIngrediendDrink = {
+  drinks: [
+    {
+      strIngredient1: 'Light rum',
+    },
+    {
+      strIngredient1: 'Applejack',
+    },
+    {
+      strIngredient1: 'Gin',
+    },
+    {
+      strIngredient1: 'Dark rum',
+    },
+    {
+      strIngredient1: 'Sweet Vermouth',
+    },
+    {
+      strIngredient1: 'Strawberry schnapps',
+    },
+  ],
+};
+
+export default mockIngrediendDrink;
---
+++ src/tests/Mocks/ingredientFood.js
@@ -0,0 +1,16 @@
+const mockIngrediendFood = {
+  meals: [
+    {
+      idIngredient: '1',
+      strIngredient: 'Chicken',
+      strDescription: 'The chicken is a type of domesticated fowl',
+      strType: null,
+    },
+    {
+      idIngredient: '2',
+      strIngredient: 'Salmon',
+      strDescription: 'Salmon is the common name for several species',
+    }],
+};
+
+export default mockIngrediendFood;
